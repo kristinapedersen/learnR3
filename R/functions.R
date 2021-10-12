2d3199d8427749b07cf30c95e41d946f0edbb2fb
Kristina Pedersen <kristina.pedersen@regionh.dk>
2021-10-12 09:25:20 +0200

    RR and ACtigraph as functions

--- R/functions.R
+++ R/functions.R
@@ -39,3 +39,53 @@ import_saliva_data <- function(file_path) {
     )
     return(saliva_data)
 }
+
+#' Import MMASH RR data
+#'
+#' @param file_path Path to RR data
+#'
+#' @return Output a data frame/Tibble
+#'
+import_RR_data <- function(file_path) {
+    RR_data <- vroom::vroom(
+        file_path,
+        col_select = -1,
+        col_types = vroom::cols(
+            ibi_s = vroom::col_double(),
+            day = vroom::col_double(),
+            time = vroom::col_time(format = "")  #if time format are giving problems this will help
+        ),
+        .name_repair = snakecase::to_snake_case
+    )
+    return(RR_data)
+}
+
+#' Import MMASH Actigraph data
+#'
+#' @param file_path Path to Actigraph data
+#'
+#' @return Output a data frame/Tibble
+#'
+import_Actigraph_data <- function(file_path) {
+    Actigraph_data <- vroom::vroom(
+        file_path,
+        col_select = -1,
+        col_types = vroom::cols(
+            axis_1 = vroom::col_double(),
+            axis_2 = vroom::col_double(),
+            axis_3 = vroom::col_double(),
+            steps = vroom::col_double(),
+            hr = vroom::col_double(),
+            inclinometer_off = vroom::col_double(),
+            inclinometer_standing = vroom::col_double(),
+            inclinometer_sitting = vroom::col_double(),
+            inclinometer_lying = vroom::col_double(),
+            vector_magnitude = vroom::col_double(),
+            day = vroom::col_double(),
+            time = vroom::col_time(format = "")
+        ),
+        .name_repair = snakecase::to_snake_case
+    )
+    return(Actigraph_data)
+}
+
