4802a0b42fc69ac8e2daeb79dedc7d7ef4278f45
Kristina Pedersen <kristina.pedersen@regionh.dk>
2021-10-12 15:28:29 +0200

    add to mmash

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -23,3 +23,31 @@ file_delete(here(c("data-raw/MMASH.zip",
 
 file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
 
+#Import datasets
+user_info_df <- import_multiple_files("user_info.csv",
+                                      import_user_info)
+
+saliva_data_df <- import_multiple_files("saliva.csv",
+                                        import_saliva_data)
+
+rr_df <- import_multiple_files("RR.csv",
+                               import_RR_data())
+
+Actigraph_df <- import_multiple_files("Actigraph.csv",
+                                      import_Actigraph_data)
+
+# Split and summarise data
+summarised_Actigraph_df <- Actigraph_df %>%
+    group_by(file_path_id, day, steps) %>%
+    summarise(across(hr,
+                     list(Mean = mean,
+                          SD = sd), na.rm = TRUE)) %>%
+    ungroup()
+
+# Split and summarise data
+summarised_rr_df <- rr_df %>%
+    group_by(day, file_path_id) %>%
+    summarise(across(ibi_s,
+                     list(Mean = mean,
+                          SD = sd), na.rm = TRUE)) %>%
+    ungroup()
